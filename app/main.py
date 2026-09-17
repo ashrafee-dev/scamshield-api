61bb59a3f7ee89bb1e61f8cd7249087be04eeb5c
Zaid Hasan <85881607+Zaid0408@users.noreply.github.com>
2026-09-17 09:45:01 +0530

    Add health endpoint in main.py (#79)

--- app/main.py
+++ app/main.py
@@ -3,3 +3,8 @@ from app.api import analyze
 
 app = FastAPI()
 app.include_router(analyze.router)
+
+# Health Check Endpoint
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
